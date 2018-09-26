894b36646a99a2811e7db08e6fa2b12dc5301310
luper <lachlann_whitehill_93@hotmail.com>
2018-09-26 15:00:32 +1000

    add initial logic

--- main.c
+++ main.c
@@ -4,8 +4,32 @@
 #include <unistd.h>
 
 int main(){
+    //Declaring all the varibles
+    //What is pid_t????
+    int x, status;
+    pid_t child;
+    x = 0;
+    child = fork();
 
-    printf("Hello\n");
+    //While??
+    if(child){
+        //Do the parent stuff
+        if(-1 == wait(&status)){
+            //do the thing after all the child processes are done
+            printf("No child process to wait for\n");
+        } else if(WIFEXITED(status)){
+            //handle exit statuses here with WEXITSTATUS(status);
+            printf("Child exit status\n"); 
+        } else {
+            //sweet sweet edge case of thread fuckerery
+            printf("shit got fucked\n");
+        }
+    } else {
+        //Do the child stuff
+        printf("here is the child process running\n");
+    }
+
+    printf("shit son it all worked!\n");
 
     return 0;
 }
